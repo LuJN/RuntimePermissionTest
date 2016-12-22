f507cd9b0292242ece410a21cd7eb290c11b159f
XingYunLiuShui <2290617605@qq.com>
2016-12-22 16:49:05 +0800

    First Commit

--- app/src/main/java/com/example/runtimepermissiontest/MainActivity.java
+++ app/src/main/java/com/example/runtimepermissiontest/MainActivity.java
@@ -6,7 +6,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -28,54 +27,37 @@ public class MainActivity extends AppCompatActivity {
         mCall.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(Build.VERSION.SDK_INT >= 23) {
-                    HandleAfterApi23();
+                if (ContextCompat.checkSelfPermission(MainActivity.this,
+                        Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
+                            Manifest.permission.CALL_PHONE)) {
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setTitle("申请权限").setMessage("该功能需要申请权限，否则无法使用")
+                                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        ActivityCompat.requestPermissions(MainActivity.this,
+                                                new String[]{Manifest.permission.CALL_PHONE}, 1);
+                                    }
+                                })
+                                .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialogInterface, int i) {
+                                        Toast.makeText(MainActivity.this, "Permission Denied",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                        builder.create().show();
+                    } else {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{Manifest.permission.CALL_PHONE}, 1);
+                    }
                 } else {
-                    HandleBeforeApi23();
+                    startActivity(mIntentCall);
                 }
+
             }
         });
-    }
-
-    /**
-     * Api23之前的事件响应
-     */
-    private void HandleBeforeApi23() {
-        startActivity(mIntentCall);
-    }
-
-    /**
-     * Api23之后的事件响应
-     */
-    private void HandleAfterApi23() {
-        if (ContextCompat.checkSelfPermission(MainActivity.this,
-                Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
-            if(ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this,
-                    Manifest.permission.CALL_PHONE)) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setTitle("申请权限").setMessage("该功能需要申请权限，否则无法使用")
-                        .setPositiveButton("确定", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                ActivityCompat.requestPermissions(MainActivity.this,
-                                        new String[]{Manifest.permission.CALL_PHONE}, 1);
-                            }
-                        })
-                        .setNegativeButton("取消", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                Toast.makeText(MainActivity.this, "Permission Denied",
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                        });
-                builder.create().show();
-            } else {
-                ActivityCompat.requestPermissions(MainActivity.this,
-                        new String[]{Manifest.permission.CALL_PHONE}, 1);
-            }
-        } else {
-            startActivity(mIntentCall);
-        }
     }
 
     /**
